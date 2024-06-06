6a255e1901e60e8387a0429653e9842739824860
theo <sdi2000190@di.uoa.gr>
2024-06-07 01:43:20 +0300

    things june_7

--- jobCommander.c
+++ jobCommander.c
@@ -16,6 +16,7 @@ int main(int argc, char** argv){
     char* N = NULL;
     char* jobID = NULL;
     char* pollState = NULL;
+    int mode = 0;
 
 
     if (argc < 4){
--- jobExecutorServer.c
+++ jobExecutorServer.c
@@ -9,15 +9,111 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <pthread.h>
+#define BUFSIZE 1024
 
 struct job{
     char* job;
-    int jobID;
-    int status; // 0: running, 1: queued
+    char* jobID;
+};
+
+struct CommanderBuffer{
+    struct job* jobBuffer;
+    int bufferSize;
+    int currentJobs;
+    pthread_mutex_t bufferMutex;
 };
 
 
-#define BUFSIZE 1024
+int addJob(char* job,struct CommanderBuffer* p){
+    struct job* newJob = malloc(sizeof(struct job));
+    newJob->job = job;
+    sprintf(newJob->jobID,"job_%d", p->currentJobs + 1);
+
+    pthread_mutex_lock(&p->bufferMutex);
+    if (p->currentJobs == p->bufferSize){
+        return -1;
+    }
+    for (int i = 0; i < p->bufferSize; i++){
+        if (p->jobBuffer[i].job == NULL){
+            p->jobBuffer[i] = *newJob;
+            p->currentJobs++;
+            return 0;
+        }
+    }
+    pthread_mutex_unlock(&p->bufferMutex);
+    return -1;
+
+}
+
+int removeJob(struct job* jobBuffer, struct job* newJob, int bufferSize){
+    for (int i = 0; i < bufferSize; i++){
+        if (jobBuffer[i].job == newJob->job){
+            jobBuffer[i].job = NULL;
+            return 0;
+        }
+    }
+    return -1;
+}
+
+
+void* workerThread(void* arg) {
+    struct CommanderBuffer* p = (struct CommanderBuffer*)arg;
+    while (1) {
+        struct job currentJob;
+        pthread_mutex_lock(&p->bufferMutex);
+        if (p->currentJobs > 0) {
+            for (int i = 0; i < p->bufferSize; i++) {
+                if (p->jobBuffer[i].job != NULL) {
+                    currentJob = p->jobBuffer[i];
+                    p->jobBuffer[i].job = NULL;
+                    p->currentJobs--;
+                    break;
+                }
+            }
+        } else {
+            pthread_mutex_unlock(&p->bufferMutex);
+            sleep(1);  // Sleep for a while if no jobs are available
+            continue;
+        }
+        pthread_mutex_unlock(&p->bufferMutex);
+
+        printf("Worker thread processing job ID: %d\n", currentJob.jobID);
+        // Process the job
+        free(currentJob.job);  // Free the job string after processing
+    }
+    return NULL;
+}
+
+
+void* controllerThread(void* arg){
+    int newsock = *((int*)arg);
+    struct CommanderBuffer* CB= ((struct CommanderBuffer*)(arg + sizeof(int)));
+    char buf[BUFSIZE];
+    while (1){
+        int n = read(newsock, buf, BUFSIZE);
+        if (n < 0){
+            perror("read");
+            exit(1);
+        }
+        if (n == 0){
+            break;
+        }
+        buf[n] = '\0';
+        printf("Server received %d bytes: %s\n", n, buf);
+    } 
+
+    if (strncmp(buf, "issueJob", 8) == 0){
+        char* job = buf + 9;
+        printf("Job: %s\n", job);
+        addJob(job, CB);
+
+
+    }
+}
+
+
+
+
 
 int main(int argc, char** argv){
     int sock, newsock ;
@@ -31,9 +127,20 @@ int main(int argc, char** argv){
         printf("Usage: jobExecutorServer [portNum] [bufferSize] [threadPoolSize]\n");
         exit(1);
     }
+
+    if (argv[1] < 0 || argv[2] <= 0 || argv[3] <= 0){
+        printf("Usage: jobExecutorServer [portNum] [bufferSize] [threadPoolSize]\n");
+        exit(1);
+    }
     int portNum = atoi(argv[1]);
     int bufferSize = atoi(argv[2]);
     int threadPoolSize = atoi(argv[3]);
+
+    struct CommanderBuffer* Commanderbuffer = malloc(sizeof(struct CommanderBuffer));
+    Commanderbuffer->jobBuffer = malloc(sizeof(struct job) * bufferSize);
+    Commanderbuffer->bufferSize = bufferSize;
+    Commanderbuffer->currentJobs = 0;
+    pthread_mutex_init(&Commanderbuffer->bufferMutex, NULL);
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         perror("socket");
@@ -63,21 +170,34 @@ int main(int argc, char** argv){
         }
         printf("Accepted connection from %s\n", rem->h_name);
         printf("Accepted connection from %s\n", inet_ntoa(client.sin_addr));
-        char buf[BUFSIZE];
-        while (1){
-            int n = read(newsock, buf, BUFSIZE);
-            if (n < 0){
-                perror("read");
-                exit(1);
-            }
-            if (n == 0){
-                break;
-            }
-            buf[n] = '\0';
-            printf("Server received %d bytes: %s\n", n, buf);
-        }
+
+        pthread_t controller;
+        void* arg = malloc(sizeof(int) + sizeof(struct Commanderbuffer*));
+        *((int*)arg) = newsock;
+        memcpy(arg + sizeof(int), &Commanderbuffer, sizeof(struct Commanderbuffer*));
+        pthread_create(&controller, NULL, controllerThread, arg);
+
+
+
+        // char buf[BUFSIZE];
+        // while (1){
+        //     int n = read(newsock, buf, BUFSIZE);
+        //     if (n < 0){
+        //         perror("read");
+        //         exit(1);
+        //     }
+        //     if (n == 0){
+        //         break;
+        //     }
+        //     buf[n] = '\0';
+        //     printf("Server received %d bytes: %s\n", n, buf);
+        // }
     }
 
+    for (int i = 0; i < threadPoolSize; i++) {
+        pthread_t thread;
+        pthread_create(&thread, NULL, workerThread, NULL);
+    }
 
 
 
